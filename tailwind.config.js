d39cf0beeb9b15f05622dfb833b0e12802db79f2
Ryan <119455390+ryan-west-casino@users.noreply.github.com>
2022-12-05 13:50:23 +0000

    Update tailwind.config.js
    Signed-off-by: Ryan <119455390+ryan-west-casino@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,6 @@ module.exports = {
   content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
   darkMode: "class",
   theme: {
-    mytheme: {
-        "primary": "#FBBD23",
-        "secondary": "#374151",
-        "accent": "#1FB2A6",
-        "neutral": "#191D24",
-        "base-100": "#2A303C",
-        "info": "#3ABFF8",
-        "success": "#36D399",
-        "warning": "#FBBD23",
-        "error": "#F87272",
-    },
     extend: {
       animation: {
         "fade-in-up": "fadeInUp 400ms ease",
@@ -30,6 +19,6 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-      themes: ["dark", "corporate", "synthwave", "aqua", "black", "luxury", "dracula", "business", "coffee"],
+      themes: ["synthwave", "dark", "corporate", "aqua", "black", "luxury", "dracula", "business", "coffee"],
 },
 };
