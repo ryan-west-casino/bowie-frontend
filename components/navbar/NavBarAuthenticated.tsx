5066ed355ae70d7cbd252641695af4fe7a91c6c2
Ryan <119455390+ryan-west-casino@users.noreply.github.com>
2022-12-05 14:30:36 +0000

    Update NavBarAuthenticated.tsx
    Signed-off-by: Ryan <119455390+ryan-west-casino@users.noreply.github.com>

--- components/navbar/NavBarAuthenticated.tsx
+++ components/navbar/NavBarAuthenticated.tsx
@@ -13,7 +13,7 @@ const NavBarAuthenticated = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><Link href="/">Home</Link></li>
+                            <li><Link href="/../?login=true">Home</Link></li>
                             <li><Link href="/wallet">Wallet</Link></li>
                             <li><Link href="/auth/logout">Logout</Link></li>
                         </ul>
